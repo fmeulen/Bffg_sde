8212376064f1a6b0bc5b0931abd83abc02f9b673
fmeulen <f.h.vandermeulen@tudelft.nl>
2022-02-04 16:35:45 +0100

    elimination of reverse! in pbridgeode_HFC!(D::DE)

--- examples/jansenrit/generatedata.jl
+++ examples/jansenrit/generatedata.jl
@@ -5,7 +5,7 @@ model= [:jr, :jr3][1]
 if model == :jr
   θtrue =[3.25, 100.0, 22.0, 50.0, 135.0, 0.8, 0.25, 5.0, 6.0, 0.56, 200.0, 2000.0]  # except for μy as in Buckwar/Tamborrino/Tubikanec#
   θtrue =[3.25, 100.0, 22.0, 50.0, 185.0, 0.8, 0.25, 5.0, 6.0, 0.56, 200.0, 2000.0]  # this gives bimodality
-  θtrue =[3.25, 100.0, 22.0, 50.0, 285.0, 0.8, 0.25, 5.0, 6.0, 0.56, 20.0, 200.0]  # also try this one
+  #θtrue =[3.25, 100.0, 22.0, 50.0, 285.0, 0.8, 0.25, 5.0, 6.0, 0.56, 20.0, 200.0]  # also try this one
   ℙ = JansenRitDiffusion(θtrue...)
   show(properties(ℙ))
   AuxType = JansenRitDiffusionAux
@@ -46,6 +46,9 @@ end
 #obs[1] = Observation(obstimes[1], x0, SMatrix{6,6}(1.0I), SMatrix{6,6}(Σdiagel*I));
 
 
+S = DE(Vern7())
+
+
 # remainder is checking 
 
 timegrids = set_timegrids(obs, 0.0005)
@@ -53,7 +56,6 @@ B = BackwardFilter(S, ℙ, AuxType, obs, timegrids, x0, false);
 Z = Innovations(timegrids, ℙ);
 
 # check
-#forwardguide(x0, ℙ, Z.z[1], B.Ms[1]);
 XX, ll = forwardguide(x0, ℙ, Z, B);
 
 pG = plot_all(ℙ, timegrids,XX)
@@ -74,21 +76,19 @@ savefig(joinpath(outdir,"deviations_guidedinitial.png"))
 
 # test
 S = Vern7direct();
-B= BackwardFilter(S, ℙ, AuxType, obs, timegrids, x0, false);
+@btime BackwardFilter(S, ℙ, AuxType, obs, timegrids, x0, false);
 
-@time BackwardFilter(S, ℙ, AuxType, obs, timegrids, x0, false);
-BackwardFilter(S, ℙ, AuxType, obs, timegrids, x0, false);
-@btime  BackwardFilter(S, ℙ, AuxType, obs, timegrids, x0, false);
+
 S = DE(Vern7())
 @btime  BackwardFilter(S, ℙ, AuxType, obs, timegrids, x0, false);
-S = RK4()
-@btime  BackwardFilter(S, ℙ, AuxType, obs, timegrids, x0, false);
+# S = RK4()
+# @btime  BackwardFilter(S, ℙ, AuxType, obs, timegrids, x0, false);
 
 
 using Profile
 using ProfileView
 Profile.init()
 Profile.clear()
-S = Vern7direct();
+S = DE(Vern7())#S = Vern7direct();
 @profile  BackwardFilter(S, ℙ, AuxType, obs, timegrids, x0, false);
 ProfileView.view()
